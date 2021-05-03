b8d18909b85d9568baae0465f91d08fae51cfb80
Heather Stratton <hastratton@gmail.com>
2021-05-03 18:45:17 -0400

    prove tab to nav

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,6 +1,13 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+//https://byui-cse.github.io/cse341-course/lesson03/items.json
+/*fetch('https://byui-cse.github.io/cse341-course/lesson03/items.json')
+.then(res => res.json())
+.then((out) => {
+    console.log('Output: ', out);
+}).catch(err => console.error(err));
+*/
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta03', { 
