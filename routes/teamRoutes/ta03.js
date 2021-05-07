8ce72b90db0a6b8789613b5b74869579aa686f55
Heather Stratton <hastratton@gmail.com>
2021-05-07 13:28:51 -0400

    ta03 and newProducts desc

--- routes/teamRoutes/ta03.js
+++ routes/teamRoutes/ta03.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
 //var jsonEngine = require('../../controllers/wk3');
 let items = [];
 
@@ -30,18 +32,22 @@ router.get('/', (req, res, next) => {
     res.render('pages/teamActivities/ta03', {
         title: 'Team Activity 03',
         path: '/ta03',
-        items: items// For pug, EJS 
-        //activeTA03: true, // For HBS
-        //contentCSS: true, // For HBS
+        items: items
     });
 })
-router.post('/', (req, res, next) => {
+router.post('/search', (req, res, next) => {
     const searchWord = req.body.input1;
-    console.log(searchWord);
-    const found = items.tags.filter(word => word === searchWord);
-    console.log(found);
-
-    res.render('./', {
+    //console.log(searchWord);
+    let searchString = searchWord.toString();
+    //console.log(searchString);
+    let lowerSearch = searchString.toLowerCase();
+    //console.log(lowerSearch);
+    let upperSearch = lowerSearch[0].toUpperCase() + lowerSearch.substring(1);
+    //console.log(upperSearch);
+    //console.log(items[0]);
+    let found = items.filter((word) =>
+        word.tags.includes(upperSearch));
+    res.render('pages/teamActivities/ta03', {
         title: "filtered",
         path: '/ta03',
         items:found
